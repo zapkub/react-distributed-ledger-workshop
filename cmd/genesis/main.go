a7fc2b42fb79327688948f5df9dcbaed0dbc0d29
Rungsikorn Rungsikavarnich <rungsikorn@icloud.com>
2019-01-06 16:02:11 +0700

    genesis account issuer created

--- cmd/genesis/main.go
+++ cmd/genesis/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"github.com/stellar/go/clients/horizon"
+	"net/http"
 	"os"
 
 	"github.com/stellar/go/keypair"
@@ -20,21 +23,43 @@ func check(err error) {
 
 func main() {
 	// Create new keypair
-	kp, err := keypair.Random()
+	issuerKeypair, err := keypair.Random()
 	check(err)
 	f, _ := os.Create("./key")
 	w := bufio.NewWriter(f)
 
 	// Create new file and write seed key to file
 	{
-		_, err := w.WriteString(kp.Seed())
+		_, err := w.WriteString(issuerKeypair.Seed())
 		check(err)
 		check(w.Flush())
 	}
 
 	// Ask XLM from stellar dev network
 	{
+		url := fmt.Sprintf("https://friendbot.stellar.org?addr=%s", issuerKeypair.Address())
+		resp, err := http.Get(url)
+		check(err)
+
+		if resp.StatusCode == 200 {
+			fmt.Println("Success")
+		}
+	}
+
+	// Checking balance
+	{
+		testNetClient := horizon.DefaultTestNetClient
+		account, err := testNetClient.LoadAccount(issuerKeypair.Address())
+		check(err)
+
+		for _, balance := range account.Balances {
+			fmt.Printf("%s: %s \n", balance.Code, balance.Balance)
+		}
 
 	}
+
+
+
+
 
 }
